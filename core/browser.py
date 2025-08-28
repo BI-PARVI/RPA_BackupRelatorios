374f38d62600910392ec9d97486098e3f4dd9d46
Joao Gabriel Ferreira Mendes <joao.mendes@parvi.com.br>
2025-08-28 15:22:30 -0300

    Adicionando novo browser.py

--- core/browser.py
+++ core/browser.py
@@ -1,19 +1,19 @@
-import subprocess
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from config.settings import CAMINHO_CHROME, CAMINHO_PERFIL
+from config.settings import CAMINHO_PERFIL, PASTA_DOWNLOADS
 
 class Browser:
     def __init__(self):
         self.driver = None
 
     def start(self):
-        comando = f'"{CAMINHO_CHROME}" --remote-debugging-port=9222 --user-data-dir="{CAMINHO_PERFIL}"'
-        subprocess.Popen(comando, shell=True)
-
         options = Options()
-        options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+        # usar perfil já configurado
+        options.add_argument(f'--user-data-dir={CAMINHO_PERFIL}')
+        options.add_argument("--disable-blink-features=AutomationControlled")
+
         prefs = {
+            "download.default_directory": PASTA_DOWNLOADS,
             "download.prompt_for_download": False,
             "download.directory_upgrade": True,
             "safebrowsing.enabled": True,
@@ -21,6 +21,7 @@ class Browser:
         }
         options.add_experimental_option("prefs", prefs)
 
+        # Selenium abre o Chrome direto
         self.driver = webdriver.Chrome(options=options)
         return self.driver
 
